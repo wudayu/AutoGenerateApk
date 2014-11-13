361a89b8a023a96748f516a6fe618b8835beeea1
David Wu <wudayu@gmail.com>
2014-11-13 15:54:11 +0800

    add copy apk to a single floader

--- README.md
+++ README.md
@@ -5,10 +5,14 @@ AutoGenerateApk is a linux script for generating different apks by using same pr
 
 ## How to use
 
-1)Rename the source's words which need be replaced.
+1) Rename the source's words which need be replaced.
   
-2)Delete all the projects that generated from source project.  
+2) Delete all the projects that generated from source project.  
   
-3)Copy the "ant_properties" file into the source project's root directory.  
+3) Copy the "ant_properties" file into the source project's root directory.  
 
-4)Run the "linux_script.sh" in terminal
+4) Change the variables in the file "linux_script".
+
+5) Run the "linux_script.sh" in terminal.
+
+6) When generating apks operation is over, u can run the "copy_to_one_floder.sh" to get all the released apks if u want.
--- linux_script.sh
+++ linux_script.sh
@@ -1,38 +1,51 @@
 # 定义替换标志字符串
-SYMBOL=wwwuuudddaaayyyuuu;
+SYMBOL=wwwuuudddaaayyyuuu
 # 定义原工程的名称
-ORIGIN_PROJECT=xcfcAndroid;
+ORIGIN_PROJECT=xcfcAndroid
+# 定义原工程的包名(Manifest文件中的)
+ORIGIN_PACKAGE_NAME=com\\.movitech\\.grande\\.haerbin
 # 定义Keystore文件夹名称
-KEYSTORE_FLODER=keystores;
+KEYSTORE_FLODER=keystores
 # 定义配置文件名称
 CONFIG_FILE=config_file
+# 定义配置文件列数
+COLS_COUNT=15;
+# 定义签名文件文件Index位置
+SIGN_FILE_INDEX=12
 
 
 
 echo ''
+# 删除没用的版本控制文件
+`find $ORIGIN_PROJECT -name *.svn | xargs rm -rf`
+
 # 获取配置文件行数也就是城市数目
 lines=`awk 'END{print NR}' $CONFIG_FILE`;
 # 获取配置文件列数也就是参数数目
-cols=`awk 'NR==1{print NF}' $CONFIG_FILE`;
+cols=$COLS_COUNT;
+
 
 # 按照各个城市开始循环
 for ((i=1;i<=lines;++i)) do
 	echo i = $i
 	# 新工程的文件夹名称
-	newProject=`awk "{if(NR==$i)print $1;}" $CONFIG_FILE|cut -d ' ' -f 1`;
+	newProject=`awk "{if(NR==$i)print $1;}" $CONFIG_FILE|cut -d '|' -f 1`;
 	echo newProject = $newProject;
 	# 使用原版工程源码生成新的工程
 	`cp -rf $ORIGIN_PROJECT $newProject`
+	# 替换应用包名
+	fileContainsPacakgeName=`grep $ORIGIN_PACKAGE_NAME -rl $newProject`
+	`sed -i '' "s/$ORIGIN_PACKAGE_NAME/$newProject/g" $fileContainsPacakgeName 2>/dev/null`
 	# 替换新工程中各个属性的值
 	for ((j=1;j<=cols;++j)) do
 		# val代表第i行第j列的字符数据
-		val=`awk "{if(NR==$i)print $1;}" $CONFIG_FILE|cut -d ' ' -f $j`
+		val=`awk "{if(NR==$i)print $1;}" $CONFIG_FILE|cut -d '|' -f $j`
 		# tmpIndex表示两位的数字，不足两位的数字需要在前面补0，比如02，09，12
 		tmpIndex=`printf "%02d\n" $j`;
 		echo j = $tmpIndex , val = $val
 		# 获取所有包含了需要修改的字段的文件
 		fileNeedReplace=`grep "$SYMBOL$tmpIndex" -rl $newProject`
-		if [ $j = 12 ] ;then
+		if [ $j = $SIGN_FILE_INDEX ] ;then
 			# keystore路径(对于项目内部来说的路径) # newProject
 			ksPath=..\\/$KEYSTORE_FLODER\\/$val
 			`sed -i '' "s/$SYMBOL$tmpIndex/$ksPath/g" $fileNeedReplace 2>/dev/null`
@@ -41,10 +54,13 @@ for ((i=1;i<=lines;++i)) do
 			`sed -i '' "s/$SYMBOL$tmpIndex/$val/g" $fileNeedReplace 2>/dev/null`
 		fi
 	done;
-	#`cd $newProject && android update project -p . -t android-20 && ant release && cd ..`
+	# 进入新工程目录
 	cd $newProject
-	android update project -p . -t android-20
+	# 生成build.xml等文件
+	android update project -p . -n $newProject -t android-20
+	# 打包并签名
 	ant release
+	# 退出新工程目录，回到生产目录
 	cd ..
 	echo ''
 done;
